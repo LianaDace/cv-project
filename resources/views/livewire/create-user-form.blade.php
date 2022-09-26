54d156b00473ac67edeade4d2fa27ee5082a4947
LianaDace <105364041+LianaDace@users.noreply.github.com>
2022-09-26 13:37:40 +0300

    Update create-user-form.blade.php

--- resources/views/livewire/create-user-form.blade.php
+++ resources/views/livewire/create-user-form.blade.php
@@ -1,7 +1,7 @@
 <div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Create a new resume') }}
+            {{ __('Izveidot jaunu CV') }}
         </h2>
     </x-slot>
 
